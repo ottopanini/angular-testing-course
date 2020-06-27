57a03867bd9bb7fcc0ec5acd256721ff88334da2
cpress <cpress@it4ipm.de>
2020-06-27 16:58:15 +0200

    Implementation of our First Jasmine Specification

--- src/app/courses/services/calculator.service.ts
+++ src/app/courses/services/calculator.service.ts
@@ -12,12 +12,12 @@ export class CalculatorService {
 
   }
 
-  add(n1: number, n2:number) {
+  add(n1: number, n2: number) {
     this.logger.log("Addition operation called");
     return n1 + n2;
   }
 
-  subtract(n1: number, n2:number) {
+  subtract(n1: number, n2: number) {
     this.logger.log("Subtraction operation called");
     return n1 - n2;
   }
