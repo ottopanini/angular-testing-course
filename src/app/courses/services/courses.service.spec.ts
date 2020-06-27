9a488951fd386ecf3d8e4076176a9fe0b1d4794e
cpress <cpress@it4ipm.de>
2020-06-27 17:58:27 +0200

    Angular HTTP Service Testing - Step-by-Step Implementation

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,7 @@
 import {CoursesService} from './courses.service';
 import {TestBed} from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {COURSES} from '../../../../server/db-data';
 
 describe('CoursesService', () => {
   let coursesService: CoursesService,
@@ -19,6 +20,15 @@ describe('CoursesService', () => {
   });
 
   it('should retrieve all courses', () => {
-    pending();
+    coursesService.findAllCourses().subscribe(courses => {
+      expect(courses).toBeTruthy('no courses returned');
+      expect(courses.length).toBe(12, 'incorrect number of courses');
+
+      const course = courses.find(course => course.id === 12);
+    });
+
+    const req = httpTestingController.expectOne('/api/courses');
+    expect(req.request.method).toEqual('GET');
+    req.flush({payload: Object.values(COURSES)});
   });
 });
