5ee4f3f4319b38d8912cec576150bbda18136e25
cpress <cpress@it4ipm.de>
2020-06-27 18:14:22 +0200

    Testing Angular HTTP Data Modification Services

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -2,6 +2,7 @@ import {CoursesService} from './courses.service';
 import {TestBed} from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {COURSES} from '../../../../server/db-data';
+import {Course} from '../model/course';
 
 describe('CoursesService', () => {
   let coursesService: CoursesService,
@@ -41,6 +42,18 @@ describe('CoursesService', () => {
     const request = httpTestingController.expectOne('/api/courses/12');
     expect(request.request.method).toEqual("GET");
     request.flush(COURSES[12]);
+  });
+
+  it('should save the course data', () => {
+    const changes: Partial<Course> = {titles: {description: 'Testing Course'}};
+    coursesService.saveCourse(12, changes).subscribe(course => {
+      expect(course.id).toBe(12);
+
+    });
+    const request = httpTestingController.expectOne('/api/courses/12');
+    expect(request.request.method).toEqual("PUT");
+    expect(request.request.body.titles.description).toEqual(changes.titles.description);
+    request.flush({...COURSES[12], ...changes});
   });
 
   afterEach(() => {
