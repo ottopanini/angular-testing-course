7a33ec3f41002068b2335f1b2506619577f7c1c1
cpress <cpress@it4ipm.de>
2020-06-28 19:32:50 +0200

    Angular Component Testing - DOM Interaction

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -11,6 +11,7 @@ import {setupCourses} from '../common/setup-test-data';
 describe('CoursesCardListComponent', () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,6 +21,7 @@ describe('CoursesCardListComponent', () => {
       .then(() => {
         fixture = TestBed.createComponent(CoursesCardListComponent);
         component = fixture.componentInstance;
+        el = fixture.debugElement;
       });
   }));
 
@@ -28,7 +30,11 @@ describe('CoursesCardListComponent', () => {
   });
 
   it("should display the course list", () => {
-    pending();
+    component.courses = setupCourses();
+
+    const cards = el.queryAll(By.css('.course-card'));
+    expect(cards).toBeTruthy('Could not find cards');
+    expect(cards.length).toEqual(12);
   });
 
   it("should display the first course", () => {
