b3e0c0aa7f804d7545ebea17f023922ef91a1555
cpress <cpress@it4ipm.de>
2020-06-30 23:16:48 +0200

    Angular Test Debugging - How to Trigger Change Detection?

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,11 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
-import {COURSES} from '../../../../server/db-data';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
-import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
-import {Course} from '../model/course';
 import {setupCourses} from '../common/setup-test-data';
 
 describe('CoursesCardListComponent', () => {
@@ -31,10 +28,11 @@ describe('CoursesCardListComponent', () => {
 
   it("should display the course list", () => {
     component.courses = setupCourses();
+    fixture.detectChanges();
 
     const cards = el.queryAll(By.css('.course-card'));
     expect(cards).toBeTruthy('Could not find cards');
-    expect(cards.length).toEqual(12);
+    expect(cards.length).toEqual(12, "Unexpected number of courses");
   });
 
   it("should display the first course", () => {
