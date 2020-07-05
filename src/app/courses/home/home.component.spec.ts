1892245117b9343f9da000d5d5effef111a6261a
cpress <cpress@it4ipm.de>
2020-07-05 22:14:50 +0200

    Understanding the Angular async() Test Zone

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -66,7 +66,7 @@ describe('HomeComponent', () => {
   });
 
 
-  it("should display advanced courses when tab clicked", fakeAsync(() => {
+  it("should display advanced courses when tab clicked - fakeAsync", fakeAsync(() => {
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));
     fixture.detectChanges();
 
@@ -78,6 +78,22 @@ describe('HomeComponent', () => {
     const cardTitles = fixture.debugElement.queryAll(By.css('.mat-card-title'));
     expect(cardTitles.length).toBeGreaterThan(0, 'Could not find card titels');
     expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
+  }));
+
+  it("should display advanced courses when tab clicked - async", async(() => {
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+    click(tabs[1]);
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      console.log('called whenStable');
+      const cardTitles = fixture.debugElement.queryAll(By.css('.mat-card-title'));
+      expect(cardTitles.length).toBeGreaterThan(0, 'Could not find card titels');
+      expect(cardTitles[0].nativeElement.textContent).toContain('Angular Security Course');
+    });
   }));
 });
 
