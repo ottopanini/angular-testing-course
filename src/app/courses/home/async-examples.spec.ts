4f0cb3f43fd7e94c9f5cc671455c933eedd0d7b3
cpress <cpress@it4ipm.de>
2020-07-05 19:00:34 +0200

    Understanding the Angular fakeAsync Testing Zone - now it works

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,3 +1,5 @@
+import {fakeAsync, tick} from '@angular/core/testing';
+
 describe('Async Testing Expamples', () => {
 
   it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
@@ -10,4 +12,15 @@ describe('Async Testing Expamples', () => {
       done();
     }, 1000);
   });
+
+  it('asynchronous test example - setTimeout()', fakeAsync(() => {
+    let test = false;
+    setTimeout(() => {
+      console.log('running assertions sertTimeout()');
+      test = true;
+      expect(test).toBeTruthy();
+    }, 1000);
+
+    tick(1000);
+  }));
 });
